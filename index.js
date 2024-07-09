1c3cef58875d60065541ba1c2967e125eda93239
softengineerstevens <softengineerstevens@gmail.com>
2024-07-09 05:47:19 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution in this file!
+//customerName//
+
+                    var customerName = 'bob'
+
+                    console.log(customerName);
+
+
+// upperCaseCustomerName();
+
+                   function upperCaseCustomerName(){
+                    customerName = customerName.toUpperCase();
+                   }
+                   console.log(customerName);
+
+//setBestCustomer
+                   function setBestCustomer(){
+                    bestCustomer = 'not bob'
+                   }
+//overwriteBestCustomer()
+
+function overwriteBestCustomer(newBestCustomer) {
+     bestCustomer = newBestCustomer;
+   }
+
+
+//changeLeastFavoriteCustomer()
+                    const leastFavoriteCustomer = 'John';
+                   function changeLeastFavoriteCustomer() {
+                    leastFavoriteCustomer = 'Zak'
+                    }
+                   console.log(changeLeastFavoriteCustomer);
+              
+
+
+
+
+
+
+
+
+          //  let customerName = 'bob'
+          //  function upperCaseCustomerName(){
+          //       customerName = customerName.toUpperCase();
+          //       // return customerName
+                             
+          //  }
+          //  console.log(customerName); // Outputs: 'bob'
+        
